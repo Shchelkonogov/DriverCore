6cfdbe078c74d59178e9df62f049230bcc0af009
Maksim <shchelkonogov@mail.ru>
2020-02-06 14:21:13 +0300

    Добавил комментарии

--- DriverCoreServer/src/main/java/ru/tecon/servlet/LoadConfig.java
+++ DriverCoreServer/src/main/java/ru/tecon/servlet/LoadConfig.java
@@ -7,6 +7,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * Сервлет для загрузки конфигурации объектов.
+ * Входные параметры serverName и url.
+ * ServerName для того что бы по webSocket передать
+ * запрос на нужный поключеннный клиент.
+ * url для того что бы клиент знал по какому адресу живет прибор
+ * для запроса от него конфигурации по мгновенным данным.
+ * Пример запроса: http://localhost:7001/DriverCore/loadConfig?serverName=MFK1500-1&url=192.168.1.26
+ */
 @WebServlet(urlPatterns = "/loadConfig")
 public class LoadConfig extends HttpServlet {
 
--- DriverCoreServer/src/main/java/ru/tecon/servlet/LoadInstantData.java
+++ DriverCoreServer/src/main/java/ru/tecon/servlet/LoadInstantData.java
@@ -7,6 +7,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * Сервлет для отправки запроса на загрузку мновенных данных объекта.
+ * Входные параметры serverName и url.
+ * ServerName для того что бы по webSocket передать
+ * запрос на нужный поключеннный клиент.
+ * url для того что бы клиент знал по какому адресу живет прибор
+ * для запроса от него мгновенных данных.
+ * Пример запроса: http://localhost:7001/DriverCore/loadInstantData?serverName=MFK1500-1&url=192.168.1.26
+ */
 @WebServlet(urlPatterns = "/loadInstantData")
 public class LoadInstantData extends HttpServlet {
 
