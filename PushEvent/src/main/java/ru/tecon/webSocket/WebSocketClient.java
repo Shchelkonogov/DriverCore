0a13faeb85e747cd46642a01ee0ff6e91471fe2c
Maksim <shchelkonogov@mail.ru>
2020-02-14 14:22:11 +0300

    Реализовал функция выключения webSocketClient

--- PushEvent/src/main/java/ru/tecon/webSocket/WebSocketClient.java
+++ PushEvent/src/main/java/ru/tecon/webSocket/WebSocketClient.java
@@ -8,17 +8,19 @@ import javax.websocket.*;
 import java.io.IOException;
 import java.net.URI;
 import java.nio.ByteBuffer;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @ClientEndpoint
 public class WebSocketClient {
 
-    private Logger LOG = Logger.getLogger(WebSocketClient.class.getName());
+    private Logger log = Logger.getLogger(WebSocketClient.class.getName());
 
-    private static boolean serviceStart = false;
+    private boolean restartService;
+
+    private ScheduledExecutorService service;
+    private ScheduledFuture future;
 
     private Session session;
 
@@ -41,30 +43,51 @@ public class WebSocketClient {
 
     @OnClose
     public void onClose() {
-        serviceStart = false;
-        connectToWebSocketServer();
+        if (restartService) {
+            connectToWebSocketServer();
+        }
     }
 
-    public synchronized void connectToWebSocketServer() {
-        if (!serviceStart) {
-            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
-            try {
-                URI uri = URI.create("ws://" + ProjectProperty.getServerURI() + ":" +
-                        ProjectProperty.getServerPort() + "/DriverCore/ws/" + ProjectProperty.getServerName());
-                container.connectToServer(this, uri);
+    /**
+     * Метод запускает работу webSocketClient
+     */
+    public void connectToWebSocketServer() {
+        restartService = true;
+        WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+        try {
+            URI uri = URI.create("ws://" + ProjectProperty.getServerURI() + ":" +
+                    ProjectProperty.getServerPort() + "/DriverCore/ws/" + ProjectProperty.getServerName());
+            container.connectToServer(this, uri);
 
-                ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
-                service.scheduleWithFixedDelay(() -> {
+            if ((service == null) || (service.isShutdown())) {
+                service = Executors.newSingleThreadScheduledExecutor();
+                future = service.scheduleWithFixedDelay(() -> {
                     try {
                         session.getBasicRemote().sendBinary(ByteBuffer.wrap("ping message server".getBytes()));
                     } catch (IOException e) {
-                        LOG.warning("connectToWebSocketServer error: " + e.getMessage());
+                        log.log(Level.WARNING, "ConnectToWebSocketServer error", e);
                     }
                 }, 28, 28, TimeUnit.SECONDS);
-            } catch (DeploymentException | IOException e) {
-                System.exit(-1);
             }
-            serviceStart = true;
+        } catch (DeploymentException | IOException e) {
+            log.log(Level.WARNING, "Ошибка запуска webSocketClient", e);
+            System.exit(-1);
+        }
+    }
+
+    /**
+     * Метод останавливает работу websocketClient;
+     */
+    public void stopService() {
+        future.cancel(true);
+        service.shutdown();
+
+        restartService = false;
+
+        try {
+            session.close();
+        } catch (IOException e) {
+            log.log(Level.WARNING, "Ошибка закрытия webSocketClient", e);
         }
     }
 }
