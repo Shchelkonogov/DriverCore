730204f7e812b8470f564edd6c4732a10296ae09
Maksim <shchelkonogov@mail.ru>
2020-03-03 13:56:53 +0300

    Переместил метод из другого класса

--- PushEvent/src/main/java/ru/tecon/server/EchoThread.java
+++ PushEvent/src/main/java/ru/tecon/server/EchoThread.java
@@ -1,15 +1,23 @@
 package ru.tecon.server;
 
+import ru.tecon.ControllerConfig;
+import ru.tecon.ProjectProperty;
+import ru.tecon.Utils;
 import ru.tecon.beanInterface.LoadOPCRemote;
 import ru.tecon.exception.MySocketException;
 import ru.tecon.model.DataModel;
-import ru.tecon.model.ParseDataModel;
+import ru.tecon.server.model.ParseDataModel;
 import ru.tecon.model.ValueModel;
 
 import javax.naming.NamingException;
 import java.io.*;
 import java.net.Socket;
 import java.nio.ByteBuffer;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.logging.Logger;
 
@@ -90,7 +98,7 @@ public class EchoThread extends Thread {
                                 case 1:
                                     try {
                                         messageConfirmSize = 2;
-                                        messagesCount = Utils.parse(data, result, 2, protocolVersion);
+                                        messagesCount = parse(data, result, 2, protocolVersion);
                                     } catch (Exception e) {
                                         socket.close();
                                         LOG.warning("run Parse version 1 exception. Socket close! " + e.getMessage() +
@@ -107,7 +115,7 @@ public class EchoThread extends Thread {
                                         markV2 = new byte[markSize];
                                         System.arraycopy(data, 4, markV2, 0, markSize);
 
-                                        messagesCount = Utils.parse(data, result, 4 + markSize, protocolVersion);
+                                        messagesCount = parse(data, result, 4 + markSize, protocolVersion);
                                     } catch (Exception e) {
                                         socket.close();
                                         LOG.warning("run Parse version 2 exception. Socket close! " + e.getMessage() +
@@ -310,5 +318,189 @@ public class EchoThread extends Thread {
         LOG.info("getIdentificationFailureMessage Message: " + Arrays.toString(response) +
                 " Thread: " + this.getId() + " objectName: " + objectName);
         return response;
+    }
+
+    private static int parse(byte[] data, List<ParseDataModel> parseDataModels, int startIndex, int protocolVersion) throws Exception {
+        List<String> result = new ArrayList<>();
+        List<Object> parseData;
+
+        System.out.println("data length: " + data.length);
+
+        int increment;
+        int messagesCount;
+        switch (protocolVersion) {
+            case 2:
+                messagesCount = ByteBuffer.wrap(data, 1, 2).getShort();
+                increment = 1;
+                break;
+            case 1:
+            default:
+                messagesCount = ByteBuffer.wrap(data, 1, 1).get();
+                increment = 0;
+        }
+
+        result.add("messages count: " + messagesCount);
+        result.add("");
+
+        int add;
+
+        int repeatCount = 0;
+
+        try {
+            for (int i = startIndex; i < data.length; i += add) {
+                if (repeatCount == messagesCount) {
+                    break;
+                }
+
+                parseData = new ArrayList<>();
+
+                int seconds = ((data[i] & 0xff) << 24) | ((data[i + 1] & 0xff) << 16) | ((data[i + 2] & 0xff) << 8) | (data[i + 3] & 0xff);
+                int nanoSeconds = ((data[i + 4] & 0xff) << 24) | ((data[i + 5] & 0xff) << 16) | ((data[i + 6] & 0xff) << 8) | (data[i + 7] & 0xff);
+                LocalDateTime dateTime = LocalDateTime.ofEpochSecond(seconds, nanoSeconds, ZoneOffset.UTC);
+                result.add("date: " + dateTime);
+
+                if (protocolVersion == 2) {
+                    int controllerStatus = data[i + 8] & 0xff;
+                    result.add("controller status: " + controllerStatus);
+                }
+
+                int bufferNumber = data[i + 8 + increment] & 0xff;
+                result.add("buffer number: " + bufferNumber);
+
+                int eventCode = ((data[i + 9 + increment] & 0xff) << 8) | ((data[i + 10 + increment] & 0xff) << 8) |
+                        ((data[i + 11 + increment] & 0xff) << 8) | (data[i + 12 + increment] & 0xff);
+                result.add("event code: " + eventCode);
+
+                int additionalDataSize = (data[i + 13 + increment] & 0xff);
+                result.add("additional data size: " + additionalDataSize);
+
+                add = 4 + 4 + increment + 1 + 4 + 1; // заголовок сообщения
+
+                int additionalDataStartIndex = i + add;
+
+                int addIndex = 0;
+                for (int j = 0; j < additionalDataSize; j++) {
+                    int type = (data[additionalDataStartIndex + addIndex] & 0xff);
+                    int count = (data[additionalDataStartIndex + 1 + addIndex] & 0xff);
+                    result.add("type: " + type + " count: " + count);
+
+                    add += 2; // заголовок дополнительных данных
+                    addIndex += 2;
+
+                    switch (type) {
+                        case 254: {
+                            Integer[] additionalData = new Integer[count];
+                            for (int k = 0; k < count; k++) {
+                                additionalData[k] = Byte.toUnsignedInt(data[additionalDataStartIndex + addIndex + k]);
+                            }
+                            result.add("additional data: " + Arrays.toString(additionalData));
+                            parseData.addAll(Arrays.asList(additionalData));
+                            add += count;
+                            addIndex += count;
+                            break;
+                        }
+                        case 253: {
+                            Integer[] additionalData = new Integer[count];
+                            for (int k = 0; k < count; k++) {
+                                additionalData[k] = ((data[additionalDataStartIndex + addIndex + (4 * k)] & 0xff) << 24) |
+                                        ((data[additionalDataStartIndex + addIndex + ((4 * k) + 1)] & 0xff) << 16) |
+                                        ((data[additionalDataStartIndex + addIndex + ((4 * k) + 2)] & 0xff) << 8) |
+                                        (data[additionalDataStartIndex + addIndex + ((4 * k) + 3)] & 0xff);
+                            }
+                            result.add("additional data: " + Arrays.toString(additionalData));
+                            parseData.addAll(Arrays.asList(additionalData));
+                            add += 4 * count;
+                            addIndex += 4 * count;
+                            break;
+                        }
+                        case 252: {
+                            Long[] additionalData = new Long[count];
+                            for (int k = 0; k < count; k++) {
+                                additionalData[k] = Integer.toUnsignedLong(((data[additionalDataStartIndex + addIndex + (4 * k)] & 0xff) << 24) |
+                                        ((data[additionalDataStartIndex + addIndex + ((4 * k) + 1)] & 0xff) << 16) |
+                                        ((data[additionalDataStartIndex + addIndex + ((4 * k) + 2)] & 0xff) << 8) |
+                                        (data[additionalDataStartIndex + addIndex + ((4 * k) + 3)] & 0xff));
+                            }
+                            result.add("additional data: " + Arrays.toString(additionalData));
+                            parseData.addAll(Arrays.asList(additionalData));
+                            add += 4 * count;
+                            addIndex += 4 * count;
+                            break;
+                        }
+                        case 251: {
+                            Long[] additionalData = new Long[count];
+                            for (int k = 0; k < count; k++) {
+                                additionalData[k] = ByteBuffer.wrap(data, additionalDataStartIndex + addIndex + (8 * k), 8).getLong();
+                            }
+                            result.add("additional data: " + Arrays.toString(additionalData));
+                            parseData.addAll(Arrays.asList(additionalData));
+                            add += 8 * count;
+                            addIndex += 8 * count;
+                            break;
+                        }
+                        case 250: {
+                            Float[] additionalData = new Float[count];
+                            for (int k = 0; k < count; k++) {
+                                additionalData[k] = Float.intBitsToFloat(((data[additionalDataStartIndex + addIndex + (4 * k)] & 0xff) << 24) |
+                                        ((data[additionalDataStartIndex + addIndex + ((4 * k) + 1)] & 0xff) << 16) |
+                                        ((data[additionalDataStartIndex + addIndex + ((4 * k) + 2)] & 0xff) << 8) |
+                                        (data[additionalDataStartIndex + addIndex + ((4 * k) + 3)] & 0xff));
+                            }
+                            result.add("additional data: " + Arrays.toString(additionalData));
+                            parseData.addAll(Arrays.asList(additionalData));
+                            add += 4 * count;
+                            addIndex += 4 * count;
+                            break;
+                        }
+                        case 249: {
+                            String[] additionalData = new String[count];
+                            for (int k = 0; k < count; k++) {
+                                additionalData[k] = new String(Arrays.copyOfRange(data, additionalDataStartIndex + addIndex, additionalDataStartIndex + addIndex + 1));
+                            }
+                            result.add("additional data: " + Arrays.toString(additionalData));
+                            parseData.addAll(Arrays.asList(additionalData));
+                            add += count;
+                            addIndex += count;
+                            break;
+                        }
+                        case 255:
+                        case 64:
+                        default: {
+                            Byte[] additionalData = new Byte[count];
+                            for (int k = 0; k < count; k++) {
+                                additionalData[k] = data[additionalDataStartIndex + addIndex + k];
+                            }
+                            result.add("additional data: " + Arrays.toString(additionalData));
+                            parseData.addAll(Arrays.asList(additionalData));
+                            add += count;
+                            addIndex += count;
+                        }
+                    }
+                }
+                result.add("");
+
+                List<String> parameters = ControllerConfig.getConfigNames(String.valueOf(bufferNumber), String.valueOf(eventCode));
+
+                if ((parameters != null) && (parameters.size() == parseData.size())) {
+                    Map<String, Object> mapData = new HashMap<>();
+
+                    for (int j = 0; j < parameters.size(); j++) {
+                        mapData.put(parameters.get(j), parseData.get(j));
+                    }
+
+                    parseDataModels.add(new ParseDataModel(dateTime, mapData));
+                }
+
+                repeatCount++;
+            }
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("IndexOutOfBoundsException " + e.getMessage());
+        }
+
+        System.out.println("repeatCount " + repeatCount);
+
+        Files.write(Paths.get(ProjectProperty.getLogFolder() + LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy_HH-mm-ss_SSS")) + ".txt"), result);
+
+        return repeatCount;
     }
 }
