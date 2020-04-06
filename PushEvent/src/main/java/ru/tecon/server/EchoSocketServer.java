9568445ae2c20dfb45b809674aee4c1d5db04ea0
Maksim <shchelkonogov@mail.ru>
2020-04-06 12:25:03 +0300

    Много доработок

--- PushEvent/src/main/java/ru/tecon/server/EchoSocketServer.java
+++ PushEvent/src/main/java/ru/tecon/server/EchoSocketServer.java
@@ -2,70 +2,142 @@ package ru.tecon.server;
 
 import ru.tecon.ControllerConfig;
 import ru.tecon.ProjectProperty;
+import ru.tecon.Utils;
+import ru.tecon.exception.MyServerStartException;
+import ru.tecon.traffic.Event;
+import ru.tecon.traffic.Statistic;
 import ru.tecon.instantData.InstantDataService;
+import ru.tecon.webSocket.WebSocketClient;
 
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.*;
+import java.util.concurrent.*;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
  * Сервер для приема socket сообщений от MFK-1500
  * реализация протокола pushEvents
- * Обязательный параметры на фходе
- * Путь к файлу с конфигурацией сервера C:\Programs\work\IDEA\WorkSpace\PushEvents\resources\config
- * Номер порта для прослушки socket 20100
- * Имя сервера для идентификации его в базе MFK1500-1
  */
 public class EchoSocketServer {
 
-    private static final Logger LOG = Logger.getLogger(EchoSocketServer.class.getName());
+    private static Logger log = Logger.getLogger(EchoSocketServer.class.getName());
+
+    private static final WebSocketClient webSocketClient = new WebSocketClient();
+
+    private static ConcurrentMap<String, Statistic> statistic = new ConcurrentHashMap<>();
+    private static ServerSocket serverSocket;
+
+    private static boolean closeApplication = true;
+
+    private static Event event;
 
     public static void main(String[] args) {
+        try {
+            startService(args);
+        } catch (MyServerStartException e) {
+            log.log(Level.WARNING, "server start exception:", e);
+        }
+    }
+
+    public static void startService(String... args) throws MyServerStartException {
         ProjectProperty.loadProperties(args[0]);
-        LOG.info("project properties load");
+        log.info("project properties load");
+        System.out.println("Конфигурация приложения загружена");
 
         // Парсим файл конфигурации
         try {
             if (!ControllerConfig.parsControllerConfig()) {
-                LOG.warning("error controller config load. Parse error");
-                System.exit(-1);
+                Utils.error("error controller config load. Parse error");
             }
         } catch (IOException e) {
-            LOG.warning("error controller config load message: " + e.getMessage());
-            System.exit(-1);
+            Utils.error("error controller config load message:", e);
         }
-        LOG.info("controller config load");
+        log.info("controller config load");
+        System.out.println("Конфигурация контроллера загружена");
+
+        // Запускаем webSocketClient
+        webSocketClient.connectToWebSocketServer();
+        log.info("Web socket client start");
+        System.out.println("Сервер получения данных доступен");
 
         ControllerConfig.startUploaderService();
-        LOG.info("controller config service start");
+        log.info("controller config service start");
+        System.out.println("Сервис обработки запросов конфигурации контроллера запущен");
 
         InstantDataService.startService();
+        log.info("Instant data service start");
+        System.out.println("Сервис обработки запросов на мгновенные данные запущен");
 
         // Запускаем serverSocket
-        ServerSocket serverSocket = null;
-        Socket socket = null;
+        Socket socket;
 
         try {
             serverSocket = new ServerSocket(ProjectProperty.getPort(), 100);
         } catch (IOException e) {
-            LOG.warning("error start server socket Message: " + e.getMessage());
-            System.exit(-1);
+            Utils.error("error start server socket Message:", e);
         }
 
-        LOG.info("server socket start");
+        log.info("server socket start");
+        System.out.println("Сервис приема данных по PushEvent запущен");
+        System.out.println("Сервер MFK1500 запущен");
 
-        while (true) {
+        while (!serverSocket.isClosed()) {
             try {
                 socket = Objects.requireNonNull(serverSocket).accept();
             } catch (IOException e) {
-                LOG.warning("error create socket connection Message: " + e.getMessage());
-                System.exit(-1);
+                if (!serverSocket.isClosed()) {
+                    log.log(Level.WARNING, "error create socket connection Message:", e);
+                } else {
+                    log.info("Server socket is closed");
+                }
+                continue;
             }
 
-            LOG.info("new connection from " + socket.getInetAddress().getHostAddress());
-            new EchoThread(socket, ProjectProperty.getServerName()).start();
+            log.info("new connection from " + socket.getInetAddress().getHostAddress());
+            EchoThread thread = new EchoThread(socket, ProjectProperty.getServerName());
+
+            thread.start();
         }
+    }
+
+    /**
+     * Метод останавливает работу приложения
+     */
+    public static void stopSocket() {
+        webSocketClient.stopService();
+        ControllerConfig.stopUploaderService();
+        InstantDataService.stopService();
+
+        try {
+            if (serverSocket != null) {
+                serverSocket.close();
+                statistic.forEach((k, v) -> v.close());
+            }
+        } catch (IOException e) {
+            log.log(Level.WARNING, "error with close sockets:", e);
+        }
+
+        log.info("EchoSocketServer stop");
+        System.out.println("Сервер MFK1500 остановлен");
+    }
+
+    public static Statistic getStatistic(String ip) {
+        statistic.putIfAbsent(ip, new Statistic(ip, event));
+        return statistic.get(ip);
+    }
+
+    public static void setEvent(Event event) {
+        EchoSocketServer.event = event;
+    }
+
+    public static boolean isCloseApplication() {
+        return closeApplication;
+    }
+
+    public static void setCloseApplication(boolean closeApplication) {
+        EchoSocketServer.closeApplication = closeApplication;
     }
 }
