b15cec02d60f5603ab261e5d09226e73fe270435
Maksim <shchelkonogov@mail.ru>
2020-04-07 13:50:33 +0300

    Добавил два метода пересчета байт в человеческое представление

--- PushEvent/src/main/java/ru/tecon/Utils.java
+++ PushEvent/src/main/java/ru/tecon/Utils.java
@@ -7,6 +7,8 @@ import ru.tecon.server.EchoSocketServer;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
+import java.text.CharacterIterator;
+import java.text.StringCharacterIterator;
 import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -67,5 +69,32 @@ public class Utils {
             EchoSocketServer.stopSocket();
             throw new MyServerStartException(message);
         }
+    }
+
+    public static String humanReadableByteCountBin(long bytes) {
+        long absB = bytes == Long.MIN_VALUE ? Long.MAX_VALUE : Math.abs(bytes);
+        if (absB < 1024) {
+            return bytes + " B";
+        }
+        long value = absB;
+        CharacterIterator ci = new StringCharacterIterator("KMGTPE");
+        for (int i = 40; i >= 0 && absB > 0xfffccccccccccccL >> i; i -= 10) {
+            value >>= 10;
+            ci.next();
+        }
+        value *= Long.signum(bytes);
+        return String.format("%.1f %ciB", value / 1024.0, ci.current());
+    }
+
+    public static String humanReadableByteCountSI(long bytes) {
+        if (-1000 < bytes && bytes < 1000) {
+            return bytes + " B";
+        }
+        CharacterIterator ci = new StringCharacterIterator("kMGTPE");
+        while (bytes <= -999_950 || bytes >= 999_950) {
+            bytes /= 1000;
+            ci.next();
+        }
+        return String.format("%.1f %cB", bytes / 1000.0, ci.current());
     }
 }
