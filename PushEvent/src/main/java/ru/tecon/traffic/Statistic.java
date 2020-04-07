e4768ba2dad2a649fa8bb1681524d212ff9b9195
Maksim <shchelkonogov@mail.ru>
2020-04-07 13:52:45 +0300

    Добавил блокировку, месячный трафик и изменил представление данных

--- PushEvent/src/main/java/ru/tecon/traffic/Statistic.java
+++ PushEvent/src/main/java/ru/tecon/traffic/Statistic.java
@@ -1,9 +1,12 @@
 package ru.tecon.traffic;
 
+import ru.tecon.ProjectProperty;
+import ru.tecon.Utils;
 import ru.tecon.server.EchoThread;
 
 import java.io.IOException;
 import java.net.Socket;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -16,7 +19,9 @@ public class Statistic {
     private EchoThread thread;
     private AtomicInteger inputTraffic = new AtomicInteger(0);
     private AtomicInteger outputTraffic = new AtomicInteger(0);
+    private AtomicInteger monthTraffic = new AtomicInteger(0);
     private LocalDateTime trafficStartTime = LocalDateTime.now();
+    private boolean block = false;
 
     private Event event;
 
@@ -59,24 +64,29 @@ public class Statistic {
 
     public void updateInputTraffic(int count) {
         inputTraffic.addAndGet(count);
+        monthTraffic.addAndGet(count);
+        checkTraffic();
         update();
     }
 
     public void updateOutputTraffic(int count) {
         outputTraffic.addAndGet(count);
+        monthTraffic.addAndGet(count);
+        checkTraffic();
         update();
     }
 
-    public int getInputTraffic() {
-        return inputTraffic.get();
+    public String getInputTraffic() {
+        return Utils.humanReadableByteCountBin(inputTraffic.get());
     }
 
-    public int getOutputTraffic() {
-        return outputTraffic.get();
+    public String  getOutputTraffic() {
+        return Utils.humanReadableByteCountBin(outputTraffic.get());
     }
 
-    public int getTraffic() {
-        return inputTraffic.get() + outputTraffic.get();
+    public String getTraffic() {
+        return Utils.humanReadableByteCountBin(inputTraffic.get() + outputTraffic.get()) + " из " +
+                Utils.humanReadableByteCountBin(ProjectProperty.getTrafficLimit());
     }
 
     public LocalDateTime getTrafficStartTime() {
@@ -99,9 +109,46 @@ public class Statistic {
         return ip;
     }
 
+    public String getMonthTraffic() {
+        return Utils.humanReadableByteCountBin(monthTraffic.get()) + " из " +
+                Utils.humanReadableByteCountBin(ProjectProperty.getTrafficLimit() * LocalDate.now().lengthOfMonth());
+    }
+
+    public void setBlock(boolean block) {
+        this.block = block;
+        update();
+    }
+
+    public boolean isBlock() {
+        return block;
+    }
+
     private void update() {
         if (event != null) {
             event.update();
         }
+    }
+
+    private void checkTraffic() {
+        if ((inputTraffic.get() + outputTraffic.get()) > ProjectProperty.getTrafficLimit()) {
+            block = true;
+            if ((socket != null) && !socket.isClosed()) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void clearDayTraffic() {
+        inputTraffic.set(0);
+        outputTraffic.set(0);
+        block = false;
+    }
+
+    public void clearMonthTraffic() {
+        monthTraffic.set(0);
     }
 }
