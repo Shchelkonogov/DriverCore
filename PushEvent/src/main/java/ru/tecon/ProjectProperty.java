3c570336ebc48c7c9258de0407b498d781d5e419
Maksim <shchelkonogov@mail.ru>
2021-03-09 17:04:39 +0300

    Убрал warning

--- PushEvent/src/main/java/ru/tecon/ProjectProperty.java
+++ PushEvent/src/main/java/ru/tecon/ProjectProperty.java
@@ -2,7 +2,6 @@ package ru.tecon;
 
 import ru.tecon.exception.MyServerStartException;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
